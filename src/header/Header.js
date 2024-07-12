4fba01885f30ebe183e412217cfd7d3429b8768d
developer <developer@developer.ru>
2024-07-12 13:30:58 +0300

    адаптив мобилки хедера и калькулятора

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,16 +1,16 @@
 import parse from "html-react-parser";
 import Picture from "../constants/Picture";
 
-export default function Header({ headerTitle, headerDesc, imageLogo }) {
+export default function Header({ title, desc, logo }) {
     return (
         <header className={"header"}>
             <div className={"header__container"}>
                 <a className={"header__logo"} href={"index.html"}>
                     {/*<Picture imgAttr={imageLogo.imgAttr}/> разложенный вариант*/}
-                    <Picture {...imageLogo}/>
+                    <Picture {...logo}/>
                 </a>
-                <h1 className={"header__title"}>{headerTitle}</h1>
-                <p className={"header__desc"}>{parse(headerDesc)}</p>
+                <h1 className={"header__title"}>{title}</h1>
+                <p className={"header__desc"}>{parse(desc)}</p>
             </div>
         </header>
     );
